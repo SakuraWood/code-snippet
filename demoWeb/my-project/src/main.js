b5e965929476738181b3a6435c47b5e47acb72e2
leichangchun <springhubei@163.com>
2017-04-25 16:24:09 +0800

    第一个vue单页面应用实例

---
+++ demoWeb/my-project/src/main.js
@@ -0,0 +1,49 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+
+import App from './App'
+import Home from './components/Home'
+import Sidebar from './components/Sidebar'
+import store from './store'
+import TimeEntries from './components/TimeEntries'
+import 'bootstrap/dist/css/bootstrap.css'
+
+// 如果在一个模块化工程中使用它，必须要通过 Vue.use() 明确地安装路由功能
+Vue.use(VueRouter)
+Vue.use(VueResource)
+
+// Vue.config.productionTip = false
+
+//路由数组
+const routes = [{
+  path : '/',
+  component : Home
+},{
+  path : '/home',
+  component : Home
+},{
+  path : '/time-entries',
+  component : TimeEntries,
+  children : [{
+    path : 'log-time',
+    // 懒加载
+    component : resolve => require(['./components/LogTime.vue'],resolve),
+  }]
+}];
+
+const router = new VueRouter({
+  routes
+});
+
+/* eslint-disable no-new */
+var app = new Vue({
+  el: '#app',
+  router,
+  store,
+  ...App,
+  // template: '<App/>',
+  // components: { App }
+});
--- vueDemo/js/component.js
+++ vueDemo/js/component.js
@@ -3,17 +3,50 @@ Vue.component('my-component',{
   template : '<div>My first component!</div>'
 });
 
-//创建根实例
-new Vue({
-  el : '#example-1',
-  //定义局部的组件
-  components : {
-    'componentfor1' : {
-      template : '<div>局部的component!</div>'
+Vue.component('my-button',{
+  //组件中定义数据，只能用函数的形式
+  data : function(){
+    return {
+      counter : 0
+    };
+  },
+  template : '<button v-on:click="increment">点击的次数 :{{counter}}</button>',
+  methods : {
+    increment : function(){
+      //此处的this 是VueComponent
+      this.counter += 1;
+      //触发事件事件。附加参数都会传给监听器回调
+      this.$emit('increment');
     }
   }
 });
 
+//创建实例
 new Vue({
-  el : '#example-3'
+  el : '#example-1',
+  data : {
+    inputvalue : ''
+  },
+  //定义局部的组件
+  components : {
+    'componentfor1' : {
+      template : '<div><div>局部的component! 他的props : {{myProps}} 动态绑定的props为 ：{{vBindProps}}</div><input v-model="vBindProps"></input></div>',
+      //要让子组件使用父组件的数据，我们需要通过子组件的props选项,子组件要显式地用 props 选项声明它期待获得的数据
+      props : ['myProps','vBindProps']
+    }
+  }
+});
+
+
+new Vue({
+  el : '#example-3',
+  data : {
+    count : 0
+  },
+  methods : {
+    incrementForEvent : function(){
+      //此处的this 是Vue实例
+      this.count += 1;
+    }
+  }
 });
