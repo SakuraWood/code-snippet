e11dd7b58ce2210ccc880065104105687e18bd16
leichangchun <springhubei@163.com>
2017-05-05 18:05:17 +0800

    jquery dom 操作复习

---
+++ notes/src/jQuery-DOM-Dome.js
@@ -0,0 +1,70 @@
+$(function(){
+  var showareaDom = $('.main-area');
+  var addbtnDom = $('#addbtn');
+  var addcontent = $('#addcontent');
+  var changeindex = $('#changeindex');
+  var newcontent = $('#newcontent');
+  var changebtnDom = $('#changebtn');
+  var insertbtnDom = $('#insertbtn');
+  var deleteindex = $('#deleteindex');
+  var deletebtnDom = $('#deletebtn');
+
+  addbtnDom.click(function(){
+    if(addcontent.val() === ''){
+      alert('请输入任务内容！');
+    }else {
+      var newDom = $('<li>' + addcontent.val() + '</li>');
+      //在showareaDom对象内部后面 增加 一个 li对象
+      showareaDom.append(newDom);
+
+      addcontent.val('');
+    }
+    changeTitle();
+  });
+
+  changebtnDom.click(function(){
+
+    if(changeindex === ""||newcontent === ""){
+      alert('请输入修改任务的序号和内容！');
+    }else{
+      $('ol.main-area li').eq(changeindex.val()-1).html(newcontent.val());
+    }
+  });
+
+
+  insertbtnDom.click(function(){
+    var insertindex1 = $('#insert1');
+    var insertindex2 = $('#insert2');
+    if(insertindex1.val()===''||insertindex2.val()===''){
+      alert("请输入需交换的任务序号！");
+    }else {
+
+    }
+
+  });
+
+  deletebtnDom.click(function(){
+    var index = deleteindex.val();
+    if(index === ''){
+      alert("请输入需删除的任务序号！");
+    }else{
+      //eq过滤中使用变量 需要用"+"号
+      //remove 移除元素
+      $('.main-area li').remove('li:eq('+ (index-1) +')');
+      deleteindex.val('');
+    }
+    changeTitle();
+  });
+
+  function changeTitle(){
+    if($('ol.main-area li').length > 0){
+      $('ol.main-area h3').html("未完成任务列表");
+    }else(
+      $('ol.main-area h3').html("任务列表为空！")
+    );
+  }
+
+  function domSwitch(parentDom,index1,index2){
+      
+  }
+});
