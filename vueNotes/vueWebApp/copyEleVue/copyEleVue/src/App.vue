c7ae052304d3676bf7b94a4e2117d2997e6873ca
leichangchun <l495735915@163.com>
2017-08-07 10:44:24 +0800

    高仿 饿了么 项目框架基本搭起来

---
+++ vueNotes/vueWebApp/copyEleVue/copyEleVue/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <ele-header></ele-header>
+    <div class="tab">
+      <div class="tab-item">
+        <router-link to="/goods">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/ratings">评价</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller">商家</router-link>
+      </div>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import header from './components/header/header'
+
+export default {
+  components: {
+    'ele-header': header
+  }
+}
+</script>
+
+<style lang="scss">
+$tab-height : 40px;
+#app{
+  width: 100%;
+
+  .tab{
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    width: 100%;
+    height:$tab-height;
+    line-height:$tab-height;
+    border: 1px rgba(7,17,27,0.1);
+
+    .tab-item{
+      -webkit-box-flex: 1;
+          -ms-flex: 1;
+              flex: 1;
+      font-size: 14px;
+      text-align: center;
+      color:rgb(77,85,93);
+      // router-link 激活状态时 自动添加class  router-link-active
+      .router-link-active{
+        color:rgb(240,20,20);
+      }
+    }
+  }
+}
+</style>
---
+++ vueNotes/vueWebApp/copyEleVue/copyEleVue/src/components/header/header.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+  <header class="header-wrapper">
+    {{msg}}
+  </header>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'this is a header!'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .header-wrapper{
+    width: 100%;
+    height: 100px;
+    background-color: #cccccc;
+  }
+</style>
