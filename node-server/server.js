9d209583f7dff8104540e89b012619f56dda59f9
leichangchun <springhubei@163.com>
2017-11-26 23:08:57 +0800

    上传图片成功

--- node-server/server.js
+++ node-server/server.js
@@ -1,11 +1,23 @@
+formidable = require('formidable') //载入formidable
 var express = require('express');
 var app = express();
 
 app.use(express.static('src',{   // 静态资源中间件
   setHeaders : function(res,path,stat){
-    res.setHeader('Cache-Control', 'max-age=' + 60);
+    res.setHeader('Cache-Control', 'max-age=' + 6000);
   }
 }))
+
+app.post('/upload',function(req,res){
+  var form = new formidable.IncomingForm();
+  form.encoding = 'utf-8';
+  form.uploadDir = './src/images';
+  form.keepExtensions = true;
+
+  res.send({
+    'msg':'upload file'
+  });
+})
 
 var server = app.listen(8081, function(){
   console.log('服务器已启动!');
