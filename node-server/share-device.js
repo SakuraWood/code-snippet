cfec8da29bec3970e19739884be667b77c794cc8
leichangchun <l495735915@163.com>
2017-11-29 17:29:49 +0800

    sync notes

--- node-server/share-device.js
+++ node-server/share-device.js
@@ -50,6 +50,60 @@ app.post('/getDotList', function(req, res) {
   });
 });
 
+app.post('/getDotInfo', function(req, res) {
+  res.send({
+    "t":"vDotInfo",
+    "data":{
+        "dotInfo":{
+            "traderId": "88",//所属运营商
+            "dotId": "878",//设备点id
+            "dotName": "逗号公寓珠海柠溪路店",//设备点的名字
+            "address": "珠海市香洲区柠溪路338号莫泰酒店7楼",//地址信息
+        "location": "逗号公寓7楼",//位置信息，具体到几楼、房间号
+		"dotType":"XY01",//新建设备点需要设置设备点的设备类型
+        "deviceAmount": "3",//设备数量
+		"spareDeviceNum": "4",//空闲设备数
+        "tel": "18888888888",//管理员联系电话
+        "isKeep": "0",//是否收藏
+        "latitude": "22.271008",//纬度
+        "longitude": "113.567912",//精度
+        "bookTime": "10"//预约可以保留时间
+        },
+        "deviceList":[
+            {
+                "uuid": "xy14781",//设备id
+                "mid": "1010",//设备类型
+                "typeName": "设备名称",//机型名
+                "typeSerial": "SXB60-1U1",//机型编号
+                "position": "07F-U02",//设备编号，在当前设备点，由用户输入
+              
+            },
+			            {
+                "uuid": "xc14782",//设备id
+                "mid": "1011",//设备类型
+                "typeName": "设备名称",//机型名
+                "typeSerial": "SXB60-1U1",//机型编号
+                "position": "07F-U02",//设备编号，在当前设备点，由用户输入
+              
+            },
+			            {
+                "uuid": "xy14783",//设备id
+                "mid": "1012",//设备类型
+                "typeName": "设备名称",//机型名
+                "typeSerial": "SXB60-1U1",//机型编号
+                "position": "07F-U02",//设备编号，在当前设备点，由用户输入
+              
+            }
+        ]
+
+    },
+    "r": "00000",
+    "msg": "操作成功",
+    "sysTime": 1496390027969
+}
+);
+});
+
 app.post('/washShopInfoPost', function(req, res) {
   res.send({
       "t": "vDotInfo",
